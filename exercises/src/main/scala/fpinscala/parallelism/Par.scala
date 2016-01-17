c6a3383f724130304515d0e7e8eeff33d6d2784c
Alessandro Zoffoli <al333z@Alessandros-Air.lan>
2016-01-17 11:10:32 +0100

    Complete parallelism exercises

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -6,7 +6,7 @@ import scala.language.implicitConversions
 
 object Par {
   type Par[A] = ExecutorService => Future[A]
-  
+
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
   def map[A, B](pa: Par[A])(f: A => B): Par[B] = es => {
@@ -19,6 +19,14 @@ object Par {
   // want the evaluation of `f` to occur in a separate thread.
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
     map(product(a, b)) { t => f(t._1, t._2) }
+
+  def flatMap[A, B](p: Par[A])(choices: A => Par[B]): Par[B] = es => {
+    val k = run(es)(p).get
+    run(es)(choices(k))
+  }
+
+  def join[A](a: Par[Par[A]]): Par[A] = es =>
+    run(es)(run(es)(a).get())
 
   def sequence[A](l: List[Par[A]]): Par[List[A]] = {
     l.foldRight(unit(List[A]())) { (a, acc) =>
@@ -55,11 +63,19 @@ object Par {
   def delay[A](fa: => Par[A]): Par[A] =
     es => fa(es)
 
+  def chooser[A, B](a: Par[A])(choices: A => Par[B]): Par[B] = es => {
+    val x: Par[Future[B]] = map(a)(ra => run(es)(choices(ra)))
+    run(es)(x).get() // !?
+  }
+
   def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
-    es => {
-      if (run(es)(cond).get) t(es) // !?
-      else f(es)
-    }
+    flatMap(cond)(if (_) t else f)
+
+  def choiceN[A](a: Par[Int])(choices: List[Par[A]]): Par[A] =
+    flatMap(a)(choices)
+
+  def choiceMap[A, B](a: Par[A])(choices: Map[A, Par[B]]): Par[B] =
+    flatMap(a)(choices)
 
   def parMap[A, B](l: List[A])(f: A => B): Par[List[B]] = fork {
     val fbs: List[Par[B]] = l.map(asyncF(f))
@@ -92,14 +108,15 @@ object Par {
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
   class ParOps[A](p: Par[A]) {}
+
 }
 
 object Examples {
   def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
     if (ints.size <= 1)
       ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
-    else { 
-      val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2) // Divide the sequence in half using the `splitAt` function.
       sum(l) + sum(r) // Recursively sum both halves and add the results together.
     }
 
